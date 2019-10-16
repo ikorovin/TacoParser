60115ab04949982e1cf53132abcab6686157f81b
Igor Korovin <54861554+ikorovin@users.noreply.github.com>
2019-10-16 12:01:29 -0500

    Complted TacoParser project

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -1,5 +1,7 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+
 
 namespace LoggingKata.Test
 {
@@ -9,13 +11,44 @@ namespace LoggingKata.Test
         public void ShouldDoSomething()
         {
             // TODO: Complete Something, if anything
+            //Arrange
+
+            var Tester = new TacoParser();
+
+            var input = "31.570771,-84.10353,Taco Bell Albany...";
+
+            //Act 
+
+            var Actual = Tester.Parse(input);
+
+
+            //Assert
+
+            Assert.NotNull(Actual);
+
         }
 
         [Theory]
-        [InlineData("Example")]
-        public void ShouldParse(string str)
+        [InlineData("31.570771,-84.10353,Taco Bell Albany...", "Taco Bell Albany..." )]
+        public void ShouldParse(string str, string expected)
         {
-            // TODO: Complete Should Parse
+            //TacoParserTests parserTest = new TacoParserTests();
+
+         //Creating an object of TacoPaser class so we can access Shouls Parse method.
+         
+            TacoParser parserInstance = new TacoParser();
+
+            // Creating an object of Tacoparser class so we can access TacoParser method.
+
+            //TacoBell newBell = new TacoBell();
+
+            //ACT
+            var actual = parserInstance.Parse(str);
+
+
+            // This is the answer to the test.
+            //Assert
+            Assert.Equal(expected, actual.Name);
         }
 
         [Theory]
@@ -24,6 +57,18 @@ namespace LoggingKata.Test
         public void ShouldFailParse(string str)
         {
             // TODO: Complete Should Fail Parse
+            //Arrange
+            var fail = new TacoParser();
+
+            //Act
+            var actual = fail.Parse(str);
+
+            //Assert
+
+            Assert.Null(actual);
+
+
+
         }
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -9,10 +9,60 @@
         
         public ITrackable Parse(string line)
         {
+            if (line == null)
+            {
+                return null;
+
+            }
             logger.LogInfo("Begin parsing");
 
-            // Do not fail if one record parsing fails, return null
-            return null; // TODO Implement
+            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+            var cells = line.Split(',');
+
+            // If your array.Length is less than 3, something went wrong
+            if (cells.Length < 3)
+            {
+                logger.LogInfo("Array is less than 3");
+                return null;
+            }
+            //double.TryParse(cells[0], out double Latitude);
+            //double.TryParse(cells[1], out double Longitude);
+            string name = cells[2];
+            if(double.TryParse(cells[0], out double Latitude) == false) 
+            {
+                logger.LogInfo("Latitude failed");
+                return null;
+            }
+            if (double.TryParse(cells[1], out double Longitute) == false)
+            {
+                logger.LogInfo("Longitude fail");
+                return null;
+            }
+                           
+            var tacoBell1 = new TacoBell();
+            tacoBell1.Name = name;
+            Point Location = new Point();
+            Location.Latitude = Latitude;
+            Location.Longitude = Longitute;
+            tacoBell1.Location = Location;
+            return tacoBell1;
+            // grab the latitude from your array at index 0
+            // grab the longitude from your array at index 1
+            // grab the name from your array at index 2
+
+                // Your going to need to parse your string as a `double`
+                // which is similar to parsing a string as an `int`
+
+                // You'll need to create a TacoBell class
+                // that conforms to ITrackable
+
+                // Then, you'll need an instance of the TacoBell class
+                // With the name and point set correctly
+
+                // Then, return the instance of your TacoBell class
+                // Since it conforms to ITrackable
+                // Do not fail if one record parsing fails, return null
+                // TODO Implement
         }
     }
 }
